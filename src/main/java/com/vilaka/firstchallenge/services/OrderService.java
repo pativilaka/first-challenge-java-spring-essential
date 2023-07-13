bd5c84709ac3259ec42be7eb81610580287b75a8
Alexandre Fernandes da Silva <fullface@alexandrefs.com>
2023-07-12 22:40:46 -0300

    Added Lombok Support to Project and Refactored the Order DTO using Lombo @Data and @AllArgsConstructor

--- src/main/java/com/vilaka/firstchallenge/services/OrderService.java
+++ src/main/java/com/vilaka/firstchallenge/services/OrderService.java
@@ -1,10 +1,12 @@
 package com.vilaka.firstchallenge.services;
 
 import com.vilaka.firstchallenge.entities.Order;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class OrderService {
 
     @Autowired
